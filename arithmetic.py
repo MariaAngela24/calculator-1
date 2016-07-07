1612191956db1333f0bc6e04203d5d1c1f64c460
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 12:09:16 -0700

    Fixed division function to return floating point

--- arithmetic.py
+++ arithmetic.py
@@ -7,8 +7,10 @@ def subtract(num1, num2):
 def multiply(num1, num2):
     return num1 * num2
 
-#def divide(num1, num2):
-#   return num1 / num2
+def divide(num1, num2):
+    float_num1 = float(num1)
+    float_num2 = float(num2)
+    return float_num1 / float_num2
 
 def square(num1):
     pass
